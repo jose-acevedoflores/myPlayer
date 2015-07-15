6080bd2486073821dddc1c3321be3f175cfe746e
Jose Acevedo <acevedoflores.jose@gmail.com>
2015-07-14 21:03:29 -0700

    A miracle just happened . Fixed header and footer. WUT

--- reactapp/app/components/MainApp/MainApp.jsx
+++ reactapp/app/components/MainApp/MainApp.jsx
@@ -22,6 +22,20 @@ var App = React.createClass({
   </div>
 </nav>
 
+<nav className="fixed-nav-bar-bottom">
+  <div id="menu" className="menu">
+    <a className="sitename" href="fixed-navigation-bar.html">Fixed Nav Bar</a>
+
+    <ul className="menu-items">
+      <li><a href="//sixrevisions.com">Homeb</a></li>
+      <li><a href="//sixrevisions.com/about/">Aboutb</a></li>
+      <li><a href="//sixrevisions.com/contact/">Contactb</a></li>
+      <li><a href="//twitter.com/sixrevisions">Twitterb</a></li>
+      <li><a href="//www.facebook.com/sixrevisions">Facebookb</a></li>
+  </ul>
+  </div>
+</nav>
+
 <section className="content">
   <div className="description">
     <h1>Fixed Navigation Bar</h1>
