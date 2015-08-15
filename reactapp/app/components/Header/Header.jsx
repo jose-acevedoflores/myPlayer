74eb72954d0a03c2ae2ba6cf79ccedd8e7489dd5
Jose Acevedo <acevedoflores.jose@gmail.com>
2015-08-14 23:20:12 -0700

    Got tabs to change on click

--- reactapp/app/components/Header/Header.jsx
+++ reactapp/app/components/Header/Header.jsx
@@ -4,23 +4,30 @@ require('./Header.css')
 var React = require('react')
 
 var Header = React.createClass({
-
+   getInitialState: function () {        
+        return {
+            currentTab: 1
+        };
+    },
+    handleSearch: function(){
+        this.setState({currentTab:1})
+    },
+    handlePlaylist:function(){
+        this.setState({currentTab:2})
+    },
     render: function(){
         return(
 
-               <nav className="fixed-nav-bar">
-                  <div id="menu" className="menu">
-                    <a className="sitename" href="fixed-navigation-bar.html">Fixed Nav Bar HE</a>
-
-                    <ul className="menu-items">
-                      <li><a href="//sixrevisions.com">Home</a></li>
-                      <li><a href="//sixrevisions.com/about/">About</a></li>
-                      <li><a href="//sixrevisions.com/contact/">Contact</a></li>
-                      <li><a href="//twitter.com/sixrevisions">Twitter</a></li>
-                      <li><a href="//www.facebook.com/sixrevisions">Facebook</a></li>
-                  </ul>
-                  </div>
-                </nav>
+              <nav className="fixed-nav-bar">
+              <div className="tabs"> 
+                <div className={this.state.currentTab == 1 ? "search selected" : "search"} onClick={this.handleSearch}>
+                  <a> Search </a>
+                </div>
+                <div className={this.state.currentTab == 2 ?  "playlist selected" : "playlist"} onClick={this.handlePlaylist}>
+                  <a> Playlists</a>
+                </div>
+              </div>
+              </nav>
 
         );
     }
