62d16133ecb1cd16bf31988823a4edb88fca1e59
Jose Acevedo <acevedoflores.jose@gmail.com>
2016-03-18 20:15:16 -0700

    Added individual loading gif to result items

--- reactappv2/components/ResultsItem/ResultsItem.jsx
+++ reactappv2/components/ResultsItem/ResultsItem.jsx
@@ -2,12 +2,24 @@ require('./ResultsItem.css')
 var React = require('react')
 
 var ResultsItem = React.createClass({
+    getInitialState:function(){
+        return {
+            loading_song:false
+        };
+    },
+    componentWillReceiveProps: function(nextProps) {
+        if(nextProps.currentPlaying == this.props.resultsItem.url){
+            this.setState({loading_song:false})
+        }
+    },
     handleClick: function(url_audio_to_request, song_name, result_list_index){
+        this.setState({loading_song:true})
         this.props.resultItemSelected(url_audio_to_request, song_name, result_list_index )
     },
     render: function() {
         return(
            <div className={this.props.currentPlaying == this.props.resultsItem.url ? " list-item currentPlaying" : " list-item "} onClick={this.handleClick.bind(this, this.props.resultsItem.url, this.props.resultsItem.name, this.props.result_list_index)}>
+                <i className={this.state.loading_song ? "loading" : null  }></i>
                 <img className="resultImage" src={this.props.resultsItem.thumbnail_url }/> 
                 <div className="resultInfo">
                     <div className="resultTitle">
